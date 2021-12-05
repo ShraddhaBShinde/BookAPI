ee43e4296db9a6ad9dba2d616603de0bdcde6615
ShraddhaBShinde <shraddhashinde489@gmail.com>
2021-12-05 23:20:25 +0530

    3 files added

---
+++ requirement.js
@@ -0,0 +1,28 @@
+//Requirements for our project
+//BOOK MANAGEMENT COMPANY
+
+//BOOKS
+//ISBN, title, publication date, language, no. of pages, authors[], publications[], category
+
+//AUTHORS
+//id, name, books[]
+
+//PUBLICATIONS
+//id, name, books[]
+
+//What are the APIs we need?
+//FOR BOOKS 
+//1] to get all books                           DONE
+//2] to get a book                              DONE
+//3] to get book based on category              DONE
+//4] to get a book based on language            ASSIGNMENT (DONE)
+
+//AUTHORS
+//1] get an author                              DONE      
+//2] get all authors                            DONE
+//3] get list of authors based on books         ASSIGNMENT (DONE)
+
+//PUBICATIONS
+//1] get all publications                       DONE
+//2] get specific publication                   ASSIGNMENT
+//3] get list of publications based on books    ASSIGNMENT
